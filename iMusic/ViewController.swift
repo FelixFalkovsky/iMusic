96809b74416c2c56f9fedb43c3b6496049682108
FosterFOX <felixkautz@gmail.com>
2019-11-22 11:02:32 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Felix Falkovsky on 22.11.2019.
+//  Copyright © 2019 Felix Falkovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
