009a5f914c71d382d19bec98a7c6f7eb24dc5e11
FosterFOX <felixkautz@gmail.com>
2019-11-23 20:42:36 +0300

    v1.2

---
+++ iMusic/Search/Cell/TrackCell.swift
@@ -0,0 +1,40 @@
+//
+//  TrackCell.swift
+//  iMusic
+//
+//  Created by Felix Falkovsky on 23.11.2019.
+//  Copyright © 2019 Felix Falkovsky. All rights reserved.
+//
+
+import UIKit
+
+protocol TrackCellViewModel {
+    var iconUrlString: String? { get }
+    var trackName: String { get }
+    var artistName: String { get }
+    var collectionName: String { get }
+}
+
+class TrackCell: UITableViewCell {
+    
+    static let reuseId = "TrackCell"
+    
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var collectionNameLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func set(viewModel: TrackCellViewModel) {
+        
+        trackNameLabel.text = viewModel.trackName
+        artistNameLabel.text = viewModel.artistName
+        collectionNameLabel.text = viewModel.collectionName
+       
+    }
+    
+}
