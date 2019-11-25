17f164225971ab89cd91afd4cd08e1cb514b44c2
FosterFOX <felixkautz@gmail.com>
2019-11-25 23:24:56 +0300

    Zuruck und Weiter Tracks

--- iMusic/Search/SearchViewController.swift
+++ iMusic/Search/SearchViewController.swift
@@ -94,11 +94,8 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath)
-        as! TrackCell
-        
+        let cell = table.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath) as! TrackCell
         let cellViewModel = searchViewModel.cells[indexPath.row]
-//        print("cellViewModel.previewUrl:", cellViewModel.previewUrl)
         cell.trackImageView.backgroundColor = .black
         cell.set(viewModel: cellViewModel)
 //        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
@@ -120,6 +117,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
         let trackDetailsView = Bundle.main.loadNibNamed("TrackDetailView", owner: self, options: nil)?.first as! TrackDetailView
         trackDetailsView.set(viewModel: cellViewModel)
+        trackDetailsView.delegate = self
         window?.addSubview(trackDetailsView)
     }
     
@@ -154,4 +152,38 @@ extension SearchViewController: UISearchBarDelegate {
             self.interactor?.makeRequest(request: Search.Model.Request.RequestType.getTracks(searchTerm: searchText))
         })
     }
+}
+extension SearchViewController: TrackMovingDelegate {
+    
+    private func getTrack(isForwardTrack: Bool) -> SearchViewModel.Cell? {
+        guard let indexPath = table.indexPathForSelectedRow else { return nil }
+        table.deselectRow(at: indexPath, animated: true)
+        var nextIndexPath: IndexPath!
+        if isForwardTrack {
+            nextIndexPath = IndexPath(row: indexPath.row + 1, section: indexPath.section)
+            if nextIndexPath.row == searchViewModel.cells.count {
+                nextIndexPath.row = 0
+            }
+        } else {
+            nextIndexPath = IndexPath(row: indexPath.row - 1, section: indexPath.section)
+            if nextIndexPath.row == -1 {
+                nextIndexPath.row = searchViewModel.cells.count - 1
+            }
+        }
+        table.selectRow(at: nextIndexPath, animated: true, scrollPosition: .none)
+        let cellViewModel = searchViewModel.cells[nextIndexPath.row]
+        print("cellViewModel.trackName:", cellViewModel.trackName)
+        return cellViewModel
+    }
+    func moveBackForPreviousTrack() -> SearchViewModel.Cell? {
+        print("zuruck")
+        return getTrack(isForwardTrack: false)
+    }
+    
+    func moveForwardForPreviousTrack() -> SearchViewModel.Cell? {
+        print("weiter")
+        return getTrack(isForwardTrack: true)
+    }
+    
+    
 }
